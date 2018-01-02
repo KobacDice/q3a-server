f952e757e6c975af43fcb797b8ce5a78a62da294
kobacdice <sora2973das@yahoo.co.jp>
2018-01-03 03:11:29 +0900

    circle.yml modified

--- README.md
+++ README.md
@@ -17,5 +17,14 @@ or
 docker run -it -d --name q3a-server-runner -p 27960:27960/udp q3a-server:latest server
 ```
 
+## docker push to aws ecs
+```
+ln -nfs circle-aws.yml circle.yml
+```
 In order to deploy AWS ECS
 you should create q3a-server-cluster and q3a-server-service
+
+## docker push to gcp gcr
+```
+ln -nfs circle-gcp.yml circle.yml
+```
---
+++ push-gcr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+PROJECT_NAME="work"
+CLUSTER_NAME="q3a-server-cluster"
+CLOUDSDK_COMPUTE_ZONE="asia-northeast1-a"
+DEBIAN_FRONTEND="noninteractive"
+
+# Setup Google Cloud SDK
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
+sudo echo ${ACCT_AUTH} | base64 --decode -i > ${HOME}/account-auth.json
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
+sudo /opt/google-cloud-sdk/bin/gcloud config set project ${PROJECT_NAME}
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster ${CLUSTER_NAME}
+sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
+
+# Deploy collector
+sbt collector/docker:publishLocal
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/q3a-server-cluster/collector
+
+# Deploy generator
+sbt generator/docker:publishLocal
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/q3a-server-cluster/generator
